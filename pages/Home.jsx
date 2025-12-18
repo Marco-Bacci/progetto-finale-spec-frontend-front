59d714f324f4fb08d89ba523adc254de8ae5a834
Marco Bacci <marcobacci94@gmail.com>
2025-12-18 16:39:20 +0100

    Feat: create static card

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -12,8 +12,39 @@ const Home = () => {
         </div>
       </div>
       <div className="container">
-        
-
+        <div className="row">
+          <div className="col-12">
+            <h2>I nostri prodotti</h2>
+          </div>
+          <div className="row">
+            <div className="col-4">
+              <div className="card mb-3 p-3 product-card" style={{maxWidth: "540px"}}>
+                <div className="row g-0">
+                  <div className="col-md-5">
+                    <img
+                      src="./images/beats-studio3.jpg"
+                      className="img-fluid rounded-start"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-7">
+                    <div className="card-body">
+                      <h5 className="card-title text-danger">Card title</h5>
+                      <p className="card-text">
+                       card category
+                      </p>
+                      <p className="card-text">
+                        <small className="text-body-secondary">
+                          500 € 
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
