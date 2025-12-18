1da4005df5492c94085343882e5a3f457e261737
Marco Bacci <marcobacci94@gmail.com>
2025-12-18 16:42:20 +0100

    Feat: create ProductCard component

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   return (
@@ -18,30 +19,7 @@ const Home = () => {
           </div>
           <div className="row">
             <div className="col-4">
-              <div className="card mb-3 p-3 product-card" style={{maxWidth: "540px"}}>
-                <div className="row g-0">
-                  <div className="col-md-5">
-                    <img
-                      src="./images/beats-studio3.jpg"
-                      className="img-fluid rounded-start"
-                      alt="..."
-                    />
-                  </div>
-                  <div className="col-md-7">
-                    <div className="card-body">
-                      <h5 className="card-title text-danger">Card title</h5>
-                      <p className="card-text">
-                       card category
-                      </p>
-                      <p className="card-text">
-                        <small className="text-body-secondary">
-                          500 € 
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ProductCard/>
             </div>
           </div>
         </div>
