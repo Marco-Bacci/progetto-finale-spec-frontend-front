0f0744543fe9c587a15f82f0510ac3b1fa2df14a
Marco Bacci <marcobacci94@gmail.com>
2025-12-18 13:22:09 +0100

    Feat: create Header with navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,35 @@
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header>
-      <h1>header</h1>
-    </header>
-  )
-}
+    <header className="d-flex justify-content-between align-items-center">
+      <img
+        className="logo"
+        src="/images/AudioSpecs-transparent.png"
+        alt="logo"
+      />
 
-export default Header
+      <div className="icons">
+        <Link to="/">
+          <div className="nav-item">
+            <i className="fa-solid fa-headphones"></i>
+            <span>Home</span>
+          </div>
+        </Link>
+        <Link to="/comparison">
+          <div className="nav-item">
+            <i className="fa-solid fa-code-compare"></i>
+            <span>Compare</span>
+          </div>
+        </Link>
+        <Link to="/favorites">
+          <div className="nav-item">
+            <i className="fa-solid fa-star"></i>
+            <span>Preferiti</span>
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
