77c1aa81feee50842ad287f395c133b6894326c5
Marco Bacci <marcobacci94@gmail.com>
2025-12-20 16:59:45 +0100

    Feat: static favorite page layout completed

--- pages/FavoritesPage.jsx
+++ pages/FavoritesPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
 const FavoritesPage = () => {
   return (
-    <div>
-    <h1>preferiti</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Preferiti</h1>
+        </div>
+        <div className="col-12">
+          <div className="d-flex justify-content-between align-items-center detail-card">
+            <div className="d-flex align-items-center gap-3">
+              <img
+                src="/images/beats-solo3.jpg"
+                alt="..."
+                className="product-thumb"
+              />
+              <h4 className="mb-0">Nome prodotto</h4>
+            </div>
+            <div>
+              <button className="add-to-list fs-5">dettaglio</button>
+              <button className="add-to-list fs-5">rimuovi</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FavoritesPage
+export default FavoritesPage;
